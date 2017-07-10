2071054df4bbcf1d200f4a5d6b277f258cd0492a
y0m0 <lixi.daniele@gmail.com>
2017-07-10 18:43:38 +0100

    Adds withdraw function to account - user can withdraw choosen amount from the account

--- spec/accountSpec.js
+++ spec/accountSpec.js
@@ -10,12 +10,32 @@ describe('Account', () => {
   });
 
   it('starts empty', () => {
-    expect(account.balance).toEqual(0);
+    expect(account.balance).toEqual(0.0);
   });
 
-  it('can increase balance', () => {
-    expect(account.balance).toEqual(0);
-    account.deposit(5);
-    expect(account.balance).toBeGreaterThan(0);
+  it('has a balance property', function() {
+    expect(account.balance).toBeDefined();
+  });
+
+  describe('#deposit', () => {
+
+    it('can increase the balance', () => {
+      expect(account.balance).toEqual(0.0);
+      account.deposit(5);
+      expect(account.balance).toBeGreaterThan(0.0);
+    });
+  });
+
+  describe('#withdraw', () => {
+
+    beforeEach(() => {
+      account.deposit(20);
+    });
+
+    it('can decrease the balance', () => {
+      expect(account.balance).toEqual(20.0);
+      account.withdraw(10);
+      expect(account.balance).toBeLessThan(20.0);
+    });
   });
 });
